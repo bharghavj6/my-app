2493af7c0c5bbfb5d1c5821e73206f71b23a2496
Bhargav J <bharghavj6@gmail.com>
2023-03-18 22:06:47 +0530

    first react

---
+++ styles/start.js
@@ -0,0 +1,13 @@
+function MyButton(){
+    return(
+    <button>Button</button>
+    );
+}
+export default function MyApp(){
+    return(
+        <div>
+        <h1>Welcome to my app</h1>
+        <MyButton />
+      </div>
+    );
+}
